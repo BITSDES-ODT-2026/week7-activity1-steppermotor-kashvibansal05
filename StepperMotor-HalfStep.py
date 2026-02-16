63ecf9da33bf67fdec19a878178f23efde8856c6
kashvibansal05 <kashvibansal@gmail.com>
2026-02-16 14:34:47 +0530

    Update StepperMotor-HalfStep.py

--- StepperMotor-HalfStep.py
+++ StepperMotor-HalfStep.py
@@ -1,1 +1,59 @@
+from machine import Pin
+import time
+
+in1 = Pin(5, Pin.OUT)
+in2 = Pin(14, Pin.OUT)
+in3 = Pin(18, Pin.OUT)
+in4 = Pin(19, Pin.OUT)
+
+while True:
+    in1.value(1)
+    in2.value(1)
+    in3.value(0)
+    in4.value(0)
+    time.sleep_ms(5)
+    
+    in1.value(0)
+    in2.value(1)
+    in3.value(0)
+    in4.value(0)
+    time.sleep_ms(5)
+    
+    in1.value(0)
+    in2.value(1)
+    in3.value(1)
+    in4.value(0)
+    time.sleep_ms(5)
+    
+    in1.value(0)
+    in2.value(0)
+    in3.value(1)
+    in4.value(0)
+    time.sleep_ms(5)
+    
+    in1.value(0)
+    in2.value(0)
+    in3.value(1)
+    in4.value(1)
+    time.sleep_ms(5)
+    
+    in1.value(0)
+    in2.value(0)
+    in3.value(0)
+    in4.value(1)
+    time.sleep_ms(5)
+    
+    in1.value(1)
+    in2.value(0)
+    in3.value(0)
+    in4.value(1)
+    time.sleep_ms(5)
+    
+    in1.value(1)
+    in2.value(0)
+    in3.value(0)
+    in4.value(0)
+    time.sleep_ms(5)
+
+
 
